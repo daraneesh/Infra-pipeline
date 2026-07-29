b08a158581945198870f71e70328ec2c8a2b47b9
daraneesh <daraneeshp2002@gmail.com>
2026-07-30 00:30:00 +0530

    add prod files

---
+++ environments/prod/backend.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "terra-statefilestore"
+    key    = "environments/prod/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "Terraform-lock"             # Enables state locking
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+
+      region = var.region
+      
+}
