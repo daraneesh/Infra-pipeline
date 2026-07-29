56d254eeb3ac6e2a177901e1d452bf6114d02c15
daraneesh <daraneeshp2002@gmail.com>
2026-07-29 23:24:27 +0530

    add qa terrafirm files

---
+++ environments/qa/main.tf
@@ -0,0 +1,17 @@
+module "vpc" {
+    source = "../../modules/vpc"
+    vpc_cidr = var.vpc_cidr
+    subnet_cidr = var.subnet_cidr
+    subnet_az = var.subnet_az
+    env = var.env
+  
+}
+
+module "ec2" {
+    source = "../../modules/ec2"
+    subnet_id = module.vpc.subnet_id
+    ami_id = var.ami_id
+    instance_type = var.instance_type
+    ec2_count = var.ec2_count
+    env = var.env
+}
---
+++ environments/qa/terraform.tfvars
@@ -0,0 +1,12 @@
+
+region      = "us-east-1"
+env         = "qa"
+vpc_cidr    = "11.0.0.0/16"
+subnet_cidr = "11.0.1.0/24"
+subnet_az   = "us-east-1a"
+
+
+
+ami_id        = "ami-02b64aa047cb5edf5"  # Example Amazon Linux 2023 AMI for us-east-1
+instance_type = "t3.micro"
+ec2_count     = "3"
