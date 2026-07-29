c9f12c583d81270fb931329e74a3a79cad48e05f
daraneesh <daraneeshp2002@gmail.com>
2026-07-29 21:33:21 +0530

    add terraform file and jenkins file dev

---
+++ environments/dev/variable.tf
@@ -0,0 +1,48 @@
+variable "region" {
+    type = string
+    default     = "us-east-1"
+    description = "Which Region AWS Resources to be Created"
+}
+variable "vpc_cidr" {
+         type = string
+         description = " VPC CIDR"
+}
+
+variable "env" {
+         type = string
+         description = "What Env we are creating (dev, qa, production)"
+}
+
+variable "subnet_cidr" {
+         type = string
+         description = " subnet CIDR"
+}
+
+variable "subnet_az" {
+         type = string
+         description = "Availability Zone for the subnet"
+}
+variable "ami_id" {
+  type        = string
+  description = "Ami id of my EC2"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Instance Type of my EC2"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "Subnet ID of my EC2"
+}
+
+variable "ec2_count" {
+  type        = string
+  description = "No of my EC2 to be created"
+}
+
+variable "env" {
+  type        = string
+  description = "What Env we are creating (dev, qa, production)"
+}
